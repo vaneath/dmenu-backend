fb90b783eaaa9e9daab93d131d5f59f991313301
vaneath <vaneathzz@gmail.com>
2023-09-08 22:23:16 +0700

    close #2 Initialize owner model

---
+++ database/migrations/2023_09_08_145406_create_owners_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('owners', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name', 50)->nullable(false);
+            $table->string('last_name', 50)->nullable(false);
+            $table->string('profile_img_path');
+            $table->string('email', 50)->nullable(false)->unique();
+            $table->string('password', 255)->nullable(false);
+            $table->string('phone_number', 20)->nullable(false);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('owners');
+    }
+};
