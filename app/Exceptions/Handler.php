3cc6c6f5796df9e74d64d70b8d701cb2e2b11668
vaneath <vaneathzz@gmail.com>
2023-09-09 20:47:50 +0700

    close #6 Integrate laravel json api

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use LaravelJsonApi\Exceptions\ExceptionParser;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -19,6 +20,15 @@ class Handler extends ExceptionHandler
     ];
 
     /**
+     * A list of the exception types that should not be reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        JsonApiException::class,
+    ];
+
+    /**
      * Register the exception handling callbacks for the application.
      */
     public function register(): void
@@ -26,5 +36,8 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+        $this->renderable(
+            ExceptionParser::make()->renderable()
+        );
     }
 }
